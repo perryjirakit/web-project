b638f416aefdd5bd8ae3de6cb12f72727b836d0e
Jirakit <jirakit.tham@gmail.com>
2021-07-06 11:06:53 +0700

    Co-authored-by: Theerapat Muangpoon <chansk131@gmail.com>

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_login.mixins import UserMixin
 from flask_login.utils import login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug import datastructures
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, current_user
 
@@ -38,14 +39,20 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.Text)
     lastname = db.Column(db.Text)
+
     username = db.Column(db.Text, unique=True)
     password = db.Column(db.Text)
 
-    def __init__(self, firstname, lastname, username, password):
+    is_admin = db.Column(db.Boolean)
+    gmail = db.Column(db.Text)
+
+    def __init__(self, firstname, lastname, gmail, username, password, is_admin):
         self.firstname = firstname
         self.lastname = lastname
+        self.gmail = gmail
         self.username = username
         self.password = generate_password_hash(password)
+        self.is_admin = is_admin
 
 
 class Trips(db.Model):
@@ -97,6 +104,12 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # redirect to login page if users length > 0
+    # query all users
+    users = User.query.all()
+    # check users count > 0
+    if len(users) > 0:
+        return redirect(url_for("login"))
     if request.method == "POST":
         firstname = request.form.get("firstname")
         lastname = request.form.get("lastname")
@@ -105,11 +118,11 @@ def register():
         password = request.form.get("password")
         confirm_password = request.form.get("confirm-password")
         user = User(firstname=firstname, lastname=lastname,
-                    gmail=gmail, username=username, password=password)
+                    gmail=gmail, username=username, password=password, is_admin=True)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("login"))
-        print("Username : " + username + " Password : " + password)
+
     return render_template("register.html")
 
 
@@ -144,8 +157,35 @@ def addtrips():
 @app.route("/usersdata")
 @login_required
 def usersdata():
+    # if current user is not admin, redirect to home
+    if not current_user.is_admin:
+        return redirect(url_for("index"))
     users = User.query.all()
     return render_template("usersdata.html", users=users)
+
+
+@app.route("/usersdata/add", methods=["GET", "POST"])
+@login_required
+def addusers():
+    # if current user is not admin, redirect to home
+    if not current_user.is_admin:
+        return redirect(url_for("index"))
+
+    if request.method == "POST":
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        gmail = request.form.get("gmail")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm-password")
+        is_admin = request.form.get("is-admin")
+        user = User(firstname=firstname, lastname=lastname,
+                    gmail=gmail, username=username, password=password, is_admin=is_admin)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("usersdata"))
+
+    return render_template("addusers.html")
 
 
 @app.route("/tripsdata")
@@ -153,3 +193,24 @@ def usersdata():
 def tripsdata():
     trips = Trips.query.all()
     return render_template("tripsdata.html", trips=trips)
+
+
+@app.route("/viewtrips")
+@login_required
+def viewtrips():
+    return render_template("viewtrips.html")
+
+
+@app.route("/admin")
+@login_required
+def admin():
+    users = User.query.all()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if not user or not check_password_hash(user.password, password):
+            return redirect(url_for("login"))
+        login_user(user)
+        return redirect(url_for("calendar"))
+    return render_template("admin.html", users=users)
